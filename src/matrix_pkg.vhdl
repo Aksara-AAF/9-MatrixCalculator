9f957842a28c2b8433e0cd1d86fa9379534a3939
mdaffrzk1 <mdaffar105@gmail.com>
2025-12-01 10:12:10 +0700

    update folder

--- src/matrix_pkg.vhdl
+++ src/matrix_pkg.vhdl
@@ -3,14 +3,14 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 package matrix_pkg is
-    -- Definisi Konstanta
+    -- Definisi konstanta
     constant MAX_SIZE   : integer := 5;
     constant DATA_WIDTH : integer := 8;
 
-    -- Definisi Tipe Matriks 5x5 (Wadah Maksimal)
+    -- Matriks 5x5 ukuran max
     type matrix_5x5 is array (0 to MAX_SIZE - 1, 0 to MAX_SIZE - 1) of signed(DATA_WIDTH - 1 downto 0);
 
-    -- Definisi Opcode (Microcode) - Sesuai Modul 9
+    -- Opcode
     constant OP_ADD       : std_logic_vector(3 downto 0) := "0001";
     constant OP_SUB       : std_logic_vector(3 downto 0) := "0010";
     constant OP_MUL       : std_logic_vector(3 downto 0) := "0011";
